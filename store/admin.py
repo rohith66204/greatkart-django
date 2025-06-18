8737162e15d6bde73e16d88a123ecd0386c92aea
Rohith Kumar <156060449+rohith66204@users.noreply.github.com>
2025-06-18 22:54:03 +0530

    review and rating

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from. models import Product
-from .models import Variation
+from .models import Variation,ReviewRating
 
 
 # Register your models here.
@@ -14,3 +14,4 @@ class VariationAdmin(admin.ModelAdmin):
     list_filter = ('product','variation_category','variation_value','is_active')
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Variation,VariationAdmin)
+admin.site.register(ReviewRating)
